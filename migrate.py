78d48db9eaae428a3687fbd5af382041ac35bae4
Shiro-Nek0 <andres.barra04@gmail.com>
2026-03-10 23:39:44 -0300

    added image_url column to dicom table

--- migrate.py
+++ migrate.py
@@ -12,7 +12,10 @@ def upgrade_db():
             # App.py also expects unit_type, adding it to prevent future headaches
             conn.execute("ALTER TABLE products ADD COLUMN unit_type TEXT DEFAULT 'unit'")
             print("Successfully added 'unit_type' column.")
-            
+
+            conn.execute("ALTER TABLE dicom ADD COLUMN image_url TEXT;")
+            print("Successfully added 'image_url' column.")
+
             conn.commit()
             print("Migration complete. Your data is intact.")
             
